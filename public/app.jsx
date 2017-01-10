f256ef46dd817487b31e05385182e89c13426ae9
1nn0x <anthony.d.morano@gmail.com>
2017-01-10 09:53:42 -0600

    Set default properties

--- public/app.jsx
+++ public/app.jsx
@@ -1,19 +1,28 @@
-
-
 var Greeter = React.createClass(
-  {
-    render: function()
     {
-      return (
-        <div>
-            <h1>Hello React!</h1>
-            <p>This is from the component!</p>
-        </div>
-      );
-    }
-  });
+        //Set default properties
+        getDefaultProps: function () {
+            return {
+                name: 'React',
+                message: 'Default'
+            };
+        },
+        render: function () {
+            var name = this.props.name;
+            var message = this.props.message;
+            return (
+                <div>
+                    <h1>Hello {name}!</h1>
+                    <p>{message}</p>
+                </div>
+            );
+        }
+    });
+
+var firstName = "Kyle";
+var messageVar = "This is a Message";
 
 ReactDOM.render(
-  <Greeter/>,
-  document.getElementById("app")
+    <Greeter name={firstName} message={messageVar}/>,
+    document.getElementById("app")
 );
